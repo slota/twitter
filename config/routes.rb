c22e4a66bcce02396ee997d489306b2b5db3c978
John Slota <john.slota@gmail.com>
2015-12-14 21:58:34 -0700

    oauth functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
   get '/auth/twitter', as: :login
+
+  get "/auth/twitter/callback", to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
 end
