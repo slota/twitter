5bff2af86787b88852c95707a94ab9aedf06fe68
John Slota <john.slota@gmail.com>
2015-12-16 18:33:02 -0700

    able to like

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,16 @@
-class TweetsController < ApplicationController
-  def create
-    twitter_data = twitter_api(current_user)
-    twitter_data.update(params[:tweet][:message])
-    redirect_to root_path
-  end
+  class TweetsController < ApplicationController
+    def create
+      twitter_data.update(params[:tweet][:message])
+      redirect_to root_path
+    end
+
+    def update
+      twitter_data.favorite(params[:id])
+      redirect_to root_path
+    end
+
+    def twitter_data
+      twitter_api(current_user)
+    end
+
 end
