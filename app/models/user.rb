c1bb9d3698b75fe130887e4af5bdaf0e5c9074ab
John Slota <john.slota@gmail.com>
2015-12-16 23:28:00 -0700

    basic styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # include ::Client
   def self.from_omniauth(auth_info)
+
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                = auth_info.uid
       new_user.name               = auth_info.extra.raw_info.name
